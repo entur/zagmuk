e3d1f2f88566f8296e2a64f130b387a3443a3e9d
Tom Erik Støwer <testower@gmail.com>
2022-11-03 12:31:53 +0100

    Remove reportWebVitals call

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM, { createRoot } from "react-dom/client";
 import { AppShellStandalone } from "./AppShellStandalone";
-import reportWebVitals from "./reportWebVitals";
+//import reportWebVitals from "./reportWebVitals";
 
 import "./index.css";
 import { registerMicroFrontend } from "@entur/micro-frontend";
